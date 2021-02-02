aa6322430db2977229acd909310fdd02e817e979
OwOY <asd1234op@gmail.com>
2021-02-02 21:49:11 +0800

    update flask&&mongo 2021/02/02

--- flask_test.py
+++ flask_test.py
@@ -3,42 +3,32 @@ import requests
 import pandas as pd
 import json
 import numpy as np
+import pymongo
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
 
-test =[{'happy':'1',
-     'unhappy':'2'},
-        {'QQ':'3',
-     'XD':'4',
-    'ohya':'小夫',
-     'ohno':'大雄'}]
-
-tour = []
-df = pd.read_csv('tour.csv')
-df = df.to_json()
-df = json.loads(df)
-for i in range(len(df['Type'])):
-    data = {}
-    for d in df:
-        data[d]=df[d][str(i)]
-    tour.append(data)
-thistour = tour
-
 @app.route('/',methods = ['GET'])
-
 def home():
+
     return 'hello world'
 
-@app.route('/test',methods = ['GET'])
 
-def index():
-    return jsonify(test)
-
-@app.route('/tour',methods = ['GET'])
-
-def tour():
-    return jsonify(thistour)
+@app.route('/<col_num>',methods = ['GET'])
+def test(col_num):
+    col_num = str(col_num)
+    col_name_dict = {}
+    client = pymongo.MongoClient('127.0.0.1')
+    db = client['d1xz_net']
+    coll_names = db.list_collection_names(session=None)
+    i = 1
+    for col_name in coll_names:
+        col_name_dict[str(i)] = col_name
+        i += 1
+    print(col_name_dict)
+    # data = [data for data in col.find({}, {'_id':0})]
+    data = [data for data in db[col_name_dict[col_num]].find({}, {'_id':0})]
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', debug=True)
